9fd5e1af06f4752350fb15cfa8cfd51a67556503
Vishali <bvishali15b@gmail.com>
2021-05-21 16:51:58 +0530

    poweshell added

---
+++ welcome.ps1
@@ -0,0 +1,23 @@
+Write-Host "Hello Vishali"
+
+param
+(
+	[Parameter(Mandatory=$true)][String]$username,
+	[Parameter(Mandatory=$true)][String]$password,
+	[Parameter(Mandatory=$true)][String]$crnumber
+ 
+)
+
+$web_client = new-object System.Net.Webclient
+$web_client.Credentials = New-Object System.Net.NetworkCredential($username, $password)
+$response_info = $web_client.DownloadString("")
+$response_json = ConvertFrom-Json $response_info
+If($response_json.records[0].approval -ne 'approved')
+{
+	$approvalException = New-Object System.Exception "CR is not Approved"
+	Throw $approvalException
+}
+else
+{
+	Write-Output $response_json.records[0].approval
+}
